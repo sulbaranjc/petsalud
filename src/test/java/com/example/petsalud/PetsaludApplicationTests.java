0951dd40dde2ae1e2545dbbdfb84b99ce8f0631a
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-03-26 09:19:19 +0100

    commit incial del proyecto

---
+++ src/test/java/com/example/petsalud/PetsaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petsalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
